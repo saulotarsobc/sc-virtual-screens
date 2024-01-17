4071c14278e19d942a399b9e670c4bb44c1ff3c8
Saulo Costa <saulotarsobc@gmail.com>
2024-01-17 09:00:46 -0300

    :fire:

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
-# Electron + Typescript + Next
+# SC - Virtual Screens
 
 ## Use
 
 ```sh
-git clone https://github.com/saulotarsobc/electron-next-ts.git;
-cd electron-next-ts;
+git clone https://github.com/saulotarsobc/sc-virtual-screens.git;
+cd sc-virtual-screens;
 npm install;
 npm start;
 ```
-
-## Help
-
-- [Electronjs - documentation](https://www.electronjs.org/pt/docs/latest/)
-- [What is CODE SIGNING | Importance of code sign | Code sing in electron js](https://youtu.be/a27EtDuUGYg)
-- [Electron Mini Tutorials](https://youtube.com/playlist?list=PL_2VhOvlMk4XLzvGgqbmjF9PkVgUGMDcJ&si=7r5qeWiby_1d6vCr)
 
 ### NPM Commands
 
--- backend/index.ts
+++ backend/index.ts
@@ -9,47 +9,48 @@ import prepareNext from "electron-next";
 // Modules
 import { BrowserWindow, app, ipcMain, IpcMainEvent, dialog } from "electron";
 import { getWinSettings, setWinSettings } from "./store";
-import { User } from "./models";
 
 const createWindow = () => {
-	const winSize = getWinSettings();
+  const winSize = getWinSettings();
 
-	const mainWindow = new BrowserWindow({
-		height: winSize.h,
-		width: winSize.w,
-		minHeight: 500,
-		minWidth: 400,
-		webPreferences: {
-			nodeIntegration: false,
-			contextIsolation: false,
-			preload: join(__dirname, "preload.js"),
-		},
-	});
+  const mainWindow = new BrowserWindow({
+    height: winSize.h,
+    width: winSize.w,
+    minHeight: 500,
+    minWidth: 400,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: false,
+      preload: join(__dirname, "preload.js"),
+    },
+  });
 
-	mainWindow.on("resize", () => {
-		setWinSettings(mainWindow.getSize());
-	});
+  mainWindow.on("resize", () => {
+    setWinSettings(mainWindow.getSize());
+  });
 
-	mainWindow.setMenu(null);
+  mainWindow.setMenu(null);
 
-	// open devtools
-	// abre o devtools se estiver em modo de desenvolvimento
-	if (isDev) mainWindow.webContents.openDevTools();
+  // open devtools
+  // abre o devtools se estiver em modo de desenvolvimento
+  if (isDev) mainWindow.webContents.openDevTools();
 
-	mainWindow.loadURL((isDev
-		? `http://localhost:8000/`
-		: format({
-			pathname: join(__dirname, "../frontend/out/index.html"),
-			protocol: "file:",
-			slashes: true,
-		})));
-}
+  mainWindow.loadURL(
+    isDev
+      ? `http://localhost:8000/`
+      : format({
+          pathname: join(__dirname, "../frontend/out/index.html"),
+          protocol: "file:",
+          slashes: true,
+        })
+  );
+};
 
 // Prepare the frontend once the app is ready
 // Prepare o frontend quando o aplicativo estiver pronto
 app.on("ready", async () => {
-	await prepareNext("./frontend");
-	createWindow();
+  await prepareNext("./frontend");
+  createWindow();
 });
 
 // Quit the app once all windows are closed
@@ -58,20 +59,17 @@ app.on("window-all-closed", app.quit);
 
 /* ++++++++++ code ++++++++++ */
 ipcMain.on("chooseFiles", (event: IpcMainEvent) => {
-	dialog.showOpenDialog({ properties: ["openFile", "multiSelections"] })
-		.then((result: any) => {
-			event.returnValue = result;
-		}).catch((err: Error) => {
-			event.returnValue = err.message;
-		});
+  dialog
+    .showOpenDialog({ properties: ["openFile", "multiSelections"] })
+    .then((result: any) => {
+      event.returnValue = result;
+    })
+    .catch((err: Error) => {
+      event.returnValue = err.message;
+    });
 });
 
 ipcMain.on("createUser", (event: IpcMainEvent, data: {}) => {
-	User.create({ ...data })
-		.then((data: any) => {
-			event.returnValue = data;
-		})
-		.catch((e: Error) => {
-			event.returnValue = e.message;
-		})
+	console.log(data)
+  event.returnValue = "ok";
 });
