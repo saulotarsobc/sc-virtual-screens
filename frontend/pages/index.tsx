49cb5a1fbf25933af09ebe9949b1d5df35495dfb
saulotarsobc <saulotarsobc@gmail.com>
2024-01-24 13:19:56 -0300

    :fire:colors

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Index() {
       </button>
 
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded m-1"
         onClick={async () => {
           const data = await global.api.runCommand(
             "deviceinstaller64 stop usbmmidd; deviceinstaller64 remove usbmmidd"
@@ -30,7 +30,7 @@ export default function Index() {
       </button>
 
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
+        className="bg-green-500 hover:bg-gre-700 text-white font-bold py-2 px-4 rounded m-1"
         onClick={async () => {
           const data = await global.api.runCommand(
             "deviceinstaller64 enableidd 1"
@@ -42,7 +42,7 @@ export default function Index() {
       </button>
 
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
+        className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded m-1"
         onClick={async () => {
           const data = await global.api.runCommand(
             "deviceinstaller64 enableidd 0"
